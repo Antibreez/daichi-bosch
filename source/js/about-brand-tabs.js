b1e8914a24e542f7c4c87e22c1da2a216dbd5329
Anton <kiyananton@gmail.com>
2023-03-19 21:52:51 +0300

    Дополнения

--- source/js/about-brand-tabs.js
+++ source/js/about-brand-tabs.js
@@ -1,6 +1,6 @@
 import Swiper, { FreeMode } from "swiper";
 
-const $tabs = $('.about-brand__tabs');
+const $tabs = $(".about-brand__tabs");
 
 const swiper = new Swiper(".about-brand__tabs-slider", {
   modules: [FreeMode],
@@ -10,12 +10,19 @@ const swiper = new Swiper(".about-brand__tabs-slider", {
   },
 });
 
-$(window).on('scroll', function() {
+$(window).on("scroll", function () {
   const gap = $(window).outerWidth() >= 1024 ? 126 : 64;
 
   console.log(gap);
 
   const top = $tabs.offset().top - $(window).scrollTop() - gap;
 
-  top > 0 ? $tabs.removeClass('fixed') : $tabs.addClass('fixed');
-})
+  top > 0 ? $tabs.removeClass("fixed") : $tabs.addClass("fixed");
+});
+
+$(document).on("click", ".about-brand__tabs-item", function (e) {
+  if ($(e.currentTarget).hasClass("active")) return;
+
+  $(".about-brand__tabs-item").removeClass("active");
+  $(e.currentTarget).addClass("active");
+});
