df77a0ca73fdc427b9759c33b2033f00bfa02bee
Anton <kiyananton@gmail.com>
2023-03-20 10:32:26 +0300

    Правки

--- source/js/select.js
+++ source/js/select.js
@@ -5,7 +5,7 @@ import { Select2 } from "select2";
 //   minimumResultsForSearch: Infinity,
 // });
 
-$("select").each((idx, item) => {
+$(".select").each((idx, item) => {
   const ph = $(item).attr("aria-placeholder") || "Все";
 
   $(item).select2({
@@ -13,3 +13,8 @@ $("select").each((idx, item) => {
     minimumResultsForSearch: Infinity,
   });
 });
+
+// $("select").select2({
+//   placeholder: ph,
+//   minimumResultsForSearch: Infinity,
+// });
